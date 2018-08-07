57f8699d48098cf51953089e8de8d8ca53c082c6
spirit <themasterspirit@gmail.com>
2018-08-07 23:50:27 +0300

    write search dao instrumental test

--- data/src/androidTest/java/com/themasterspirit/flickr/data/SearchParamsDaoTest.kt
+++ data/src/androidTest/java/com/themasterspirit/flickr/data/SearchParamsDaoTest.kt
@@ -1,26 +1,91 @@
 package com.themasterspirit.flickr.data
 
+import androidx.room.Room
 import androidx.test.InstrumentationRegistry
 import androidx.test.runner.AndroidJUnit4
-import org.junit.Assert.assertEquals
-import org.junit.BeforeClass
+import com.themasterspirit.flickr.data.db.FlickrDatabase
+import com.themasterspirit.flickr.data.db.models.SearchParams
+import com.themasterspirit.flickr.data.db.models.SearchParamsDao
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
+import java.util.*
+
 
 @RunWith(AndroidJUnit4::class)
 class SearchParamsDaoTest {
 
-    @BeforeClass
-    fun setup() {
-        throw RuntimeException("Sorry dude, you won't find any test!")
+    private val queryFirst = "query"
+    private val querySecond = "sample text"
+
+    private val initialData: List<SearchParams> = listOf(
+            SearchParams("$queryFirst 0", Date()),
+            SearchParams("$queryFirst 1", Date()),
+            SearchParams("$querySecond 1", Date()),
+            SearchParams("$querySecond 0", Date())
+    ).sortedByDescending { it.date.time }
+
+    private lateinit var database: FlickrDatabase
+
+    private val dao: SearchParamsDao
+        get() = database.searchParamsDao()
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getTargetContext()
+        database = Room.inMemoryDatabaseBuilder(context, FlickrDatabase::class.java)
+                .allowMainThreadQueries()
+                .build()
     }
 
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        database.close()
+    }
 
     @Test
-    fun sampleTest() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+    fun testWrite() {
+        dao.insert(*initialData.toTypedArray())
+        dao.get().test()
+                .assertValues(initialData)
+                .assertNoErrors()
+    }
 
-        assertEquals("com.themasterspirit.flickr.data.test", appContext.packageName)
+    @Test
+    fun testUniqueQuery() {
+        val query = "query_text"
+        val items = listOf(SearchParams(query, Date()), SearchParams(query, Date()))
+        dao.insert(*items.toTypedArray())
+        dao.get().test()
+                .assertValue(listOf(items.first()))
+                .assertNoErrors()
+    }
+
+    @Test
+    fun testDeleting() {
+        val itemsToDelete = listOf(initialData[0], initialData[2])
+        val itemsLeft = listOf(initialData[1], initialData[3])
+
+        dao.insert(*initialData.toTypedArray())
+        dao.delete(*itemsToDelete.toTypedArray())
+
+        dao.get().test().assertValues(itemsLeft)
+    }
+
+    @Test
+    fun testSearch() {
+        dao.insert(*initialData.toTypedArray())
+
+        val searchQueryFirst: String = queryFirst.substring(0..1)
+        val searchQuerySecond: String = querySecond.substring(0..2)
+
+        val expectedResultFirst = initialData.filter { it.query.startsWith(searchQueryFirst) }
+        val expectedResultSecond = initialData.filter { it.query.startsWith(searchQuerySecond) }
+
+        dao.search(searchQueryFirst).test().assertValue(expectedResultFirst)
+        dao.search(searchQuerySecond).test().assertValue(expectedResultSecond)
     }
 }
